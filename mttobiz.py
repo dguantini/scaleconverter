2c5bb1c3c3487ffcacc8d768087e9c678594bbcc
Damiano Guantini <35350110+dguantini@users.noreply.github.com>
2018-04-05 12:30:30 +0200

    Add files via upload

---
+++ mttobiz.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+class ClassName(object):
+    """docstring for [object Object]."""
+    def __init__(self, arg):
+        super([object Object], self).__init__()
+        self.arg = arg
